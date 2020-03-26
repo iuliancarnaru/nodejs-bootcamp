317c6eba793bff782455fafe723b1da8111b5a30
iuliancarnaru <iulian.carnaru@gmail.com>
2020-03-26 17:23:31 +0000

    basic routing

--- server/http.js
+++ server/http.js
@@ -1,9 +1,22 @@
 const http = require("http");
+const url = require("url");
 
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello form the server");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("Hello to overview page");
+  } else if (pathName === "/product") {
+    res.end("Hello to product page");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "My-custom-header": "hello-world"
+    });
+    res.end("<h1>Page not found...</h1>");
+  }
 });
 
 server.listen(PORT, "127.0.0.1", () => {
