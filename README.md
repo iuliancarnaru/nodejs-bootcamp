e8c65a35cd2945e8bf7b1692f13c0f30cbcd0cbf
iuliancarnaru <iulian.carnaru@gmail.com>
2020-03-27 21:08:47 +0000

    learning about asyc await in nodejs

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # nodejs-bootcamp
 
 Master Node by building a real-world RESTful API and web app (with authentication, Node.js security, payments & more)
+
+Windows Terminal Error:
+
+...cannot be loaded because running scripts is disabled
+on this system. For more information, see about_Execution_Policies at https:/go.microsoft.com/fwlink/?LinkID=135170.
+At line:1 char:1
+
+run this line in terminal: `set-executionpolicy remotesigned`
--- asynchronus-js/index.js
+++ asynchronus-js/index.js
@@ -1,18 +1,85 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-// callback inside callback (callback hell)
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, err => {
-        if (err) return console.log(err.message);
-        console.log(`Random dog image saved to file`);
-      });
+const readFilePromise = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(`Could not find that file...`);
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject(`Could not write the file...`);
+      resolve(`File write succesfuly`);
+    });
+  });
+};
+
+const getDogPicture = async () => {
+  try {
+    const data = await readFilePromise(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    // const result = await superagent.get(
+    //   `https://dog.ceo/api/breed/${data}/images/random`
+    // );
+
+    const result1Promise = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const result2Promise = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const result3Promise = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const allPromises = await Promise.all([
+      result1Promise,
+      result2Promise,
+      result3Promise
+    ]);
+
+    const images = allPromises.map(promise => promise.body.message);
+    // console.log(result.body.message);
+
+    await writeFilePromise('dog-img.txt', images.join('\n'));
+    console.log(`Random dog images saved to file`);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+
+  return `2. Pictures are ready`;
+};
+
+(async () => {
+  try {
+    console.log(`1. Preparing to get dog pictures`);
+    let picture = await getDogPicture();
+    console.log(picture);
+    console.log(`3. Done getting dog pictures`);
+  } catch (error) {
+    console.log('ERROR...BOOM!');
+  }
+})();
+
+/* 
+readFilePromise(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then(res => {
+    console.log(res.body.message);
+    return writeFilePromise('dog-img.txt', res.body.message);
+  })
+  .then(() => console.log(`Random dog image saved to file`))
+  .catch(err => console.log(err.message));
+  */
