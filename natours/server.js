adb2112e42113f7ecf2eb94ce9ef3e3b064d9d1e
iuliancarnaru <iulian.carnaru@gmail.com>
2020-04-01 20:27:00 +0100

    add unhandeled and uncaught error handler

--- natours/server.js
+++ natours/server.js
@@ -3,6 +3,13 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: '.env.local' });
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log(`Uncaught exception! Shutting down...`);
+  // use third party app to restart the service
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -18,8 +25,18 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log(`DB connection succesful`));
+// .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, '127.0.0.1', () =>
+const server = app.listen(PORT, '127.0.0.1', () =>
   console.log(`App runnind and listening on port ${PORT}`)
 );
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log(`Unhandeled rejection! Shutting down...`);
+  // use third party app to restart the service
+  server.close(() => {
+    process.exit(1);
+  });
+});
