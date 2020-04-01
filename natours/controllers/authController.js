150bb7f76b53a62d3c116e674d454735de6eabe0
iuliancarnaru <iulian.carnaru@gmail.com>
2020-04-01 21:35:29 +0100

    adding hashing for passwords and jwt package

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -2,7 +2,13 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/cachAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const user = await User.create(req.body);
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
   res.status(201).json({
     status: 'success',
     data: {
--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,7 +25,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // don't save confirm password to DB
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
