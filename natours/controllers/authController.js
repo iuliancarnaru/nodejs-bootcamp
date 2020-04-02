3687bf799028691d46d8151fadf8212908164149
iuliancarnaru <iulian.carnaru@gmail.com>
2020-04-02 21:25:14 +0100

    updating protect function

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -73,21 +73,21 @@ exports.protect = catchAsync(async (req, res, next) => {
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // check if user still exists
-  const freshUser = await User.findById(decoded.id);
-  if (!freshUser) {
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
     return next(
       new AppError(`The user associated with this token no longer exists`, 401)
     );
   }
 
   // check if user changed password after token was issued
-  if (freshUser.changedPasswordAfter(decoded.iat)) {
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
     return next(
       new AppError(`User recently changed password. Please log in again`, 401)
     );
   }
 
   // grant access to protected route
-  req.user = freshUser;
+  req.user = currentUser;
   next();
 });
