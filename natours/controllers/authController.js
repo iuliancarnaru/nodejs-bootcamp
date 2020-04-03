9bc95af5b9184ffa61d6b23d85c2ab20dec4d1ef
iuliancarnaru <iulian.carnaru@gmail.com>
2020-04-03 16:23:57 +0100

    adding restriction for specific users

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -91,3 +91,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles is an array ['admin','lead-guide'] (default role is 'user')
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError(`You don't have permission to perform this action`, 403)
+      );
+    }
+    next();
+  };
+};
--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -12,7 +12,7 @@ const {
   getTourStats,
   getMonthlyPlan,
 } = require('../controllers/tourController');
-const { protect } = require('../controllers/authController');
+const { protect, restrictTo } = require('../controllers/authController');
 
 // MIDDLEWARE STACKS
 // router.param('id', checkId);
@@ -22,6 +22,10 @@ router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
