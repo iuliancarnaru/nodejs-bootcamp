63f728e26d38a4815351c12dbcd15d733a126ffc
iuliancarnaru <iulian.carnaru@gmail.com>
2020-04-05 12:52:42 +0100

    more security features

--- natours/app.js
+++ natours/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -7,11 +12,46 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARE
+// development login
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: `To many requests from this IP, please try again in an hour`,
+});
+// if server reset limit is reset as well
+app.use('/api', limiter);
+
+// security for http headers
+app.use(helmet());
+
+// body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against noSQL query injections
+app.use(mongoSanitize());
+
+// data sanitization against XSS
+app.use(xss());
+
+// prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+    ],
+  })
+);
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
@@ -29,7 +69,7 @@ app.all('*', (req, res, next) => {
   // res.status(404).json({
   //   status: 'fail',
   //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
+  // });F
 
   // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
   // err.status = 'fail';
