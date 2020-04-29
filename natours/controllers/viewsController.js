50e672a54934553dc0cd034e7af38b01bd2d6b01
iuliancarnaru <iulian.carnaru@gmail.com>
2020-04-29 11:39:13 +0100

    adding dynamic data to overview page

--- natours/controllers/viewsController.js
+++ natours/controllers/viewsController.js
@@ -1,11 +1,21 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('tour', {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/cachAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // get tour data from collection
+  const tours = await Tour.find();
+
+  // build template
+
+  // render template with data
+  res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
-  res.status(200).render('overview', {
+  res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
   });
 };
