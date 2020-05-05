5d4a9b86f50229e258a2c90d9b5a92338c9a982d
iuliancarnaru <iulian.carnaru@gmail.com>
2020-05-05 12:42:22 +0100

    adding images to the tour when you create or update

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,7 +1,73 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/cachAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image, please upload only images', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  {
+    name: 'imageCover',
+    maxCount: 1,
+  },
+  {
+    name: 'images',
+    maxCount: 3,
+  },
+]);
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) {
+    return next();
+  }
+
+  // cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    // 3:2 ratio
+    .resize(2000, 1333, {})
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.body.imageCover}`);
+
+  // tour images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, idx) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${idx + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333, {})
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/users/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
+  next();
+});
+
+// upload.single('image'); --> req.file
+// upload.array('images', 5); --> req.files
+// upload.fields([{...}]); --> req.files
 
 exports.aliasTopTours = (req, res, next) => {
   // pre-filling the query strings
--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -13,6 +13,8 @@ const {
   getMonthlyPlan,
   getToursWithin,
   getDistances,
+  uploadTourImages,
+  resizeTourImages,
 } = require('../controllers/tourController');
 const { protect, restrictTo } = require('../controllers/authController');
 const reviewRouter = require('./reviewRoutes');
@@ -41,7 +43,13 @@ router
 router
   .route('/:id')
   .get(getTour)
-  .patch(protect, restrictTo('admin', 'lead-guide'), updateTour)
+  .patch(
+    protect,
+    restrictTo('admin', 'lead-guide'),
+    uploadTourImages,
+    resizeTourImages,
+    updateTour
+  )
   .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
