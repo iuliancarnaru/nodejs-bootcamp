11d43c24228200d0d2dffbe62c3f50a01a07f0f0
iuliancarnaru <iulian.carnaru@gmail.com>
2020-05-06 08:33:02 +0100

    implementing stripe payments

--- natours/app.js
+++ natours/app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
+const bookingsRouter = require('./routes/bookingRoutes');
 
 const app = express();
 
@@ -82,6 +83,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/booking', bookingRouter);
 
 // Handling undefined routes
 app.all('*', (req, res, next) => {
---
+++ natours/controllers/bookingController.js
@@ -0,0 +1,35 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/cachAsync');
+const factory = require('./handlerFactory');
+const AppError = require('../utils/appError');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // get the currentlu booked toor
+  const tour = await Tour.findById(req.params.tourId);
+
+  // create checkout session
+  const session = await stripe.checkout.session.create({
+    payment_method_type: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        name: `${tour.name} Tour`,
+        description: tour.summary,
+        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        amount: tour.price * 100,
+        currency: 'usd',
+        quantity: 1,
+      },
+    ],
+  });
+
+  // create session as response
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
