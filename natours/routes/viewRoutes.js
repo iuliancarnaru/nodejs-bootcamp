7933c5231f14f9ac9f5e99c6a307bff18fd841cb
iuliancarnaru <iulian.carnaru@gmail.com>
2020-05-06 16:19:01 +0100

    adding booking model

--- natours/routes/viewRoutes.js
+++ natours/routes/viewRoutes.js
@@ -7,11 +7,13 @@ const {
   updateUserData,
 } = require('../controllers/viewsController');
 
+const { createBookingCheckout } = require('../controllers/bookingController');
+
 const { protect, isLoggedIn } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, getOverview);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
